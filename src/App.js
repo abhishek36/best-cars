84cc5a6f445d333202d596e630b746f492d2e2a6
abhishek36 <akamble9913@gmail.com>
2021-04-13 08:44:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React , {useState} from 'react'
+import './style.css'
 
 function App() {
+
+  const carsCollection = {
+    Renault: [
+      { name: "Duster", rating: "4/5" },
+      { name: "Fluence", rating: "3.5/5" }
+    ],
+    Hyundai : [
+      { name: "Verna", rating: "4.8/5" },
+      { name: "Flantra", rating: "3.8/5" }
+    ],
+    Honda: [
+      { name: "Honda City", rating: "4.5/5" },
+      { name: "Honda Civic", rating: "4/5" }
+    ]
+  };
+
+
+  var cars = Object.keys(carsCollection);
+
+  function handlebtnClick(type) {
+    setCarsToShow(type);
+  }
+
+  const [carstoShow, setCarsToShow] = useState("Renault");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Best Cars</h1>
+        <p>Check cars rating </p>
+        {cars.map((e)=>(
+          <button className="button" onClick={() => handlebtnClick(e)}>
+          {e}
+        </button>
+        ))}
+
+<ul style={{ listStyle: "none" }}>
+          {carsCollection[carstoShow].map((car) => (
+            <li>
+              <div className="list-div">
+                {car.name}
+                <div style={{ fontSize: "smaller" }}>{car.rating}</div>
+              </div>
+            </li>
+          ))}
+        </ul>
     </div>
   );
 }
